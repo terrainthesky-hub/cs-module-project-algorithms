56450c6ec6028f9c61069823ef9dabbe98cf1546
Lesley Rich <lesley.t.rich@gmail.com>
2020-09-16 15:41:21 -0700

    9/16/2020

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,22 +1,73 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
 '''
+
+# at every index we multiply all the other numbers
+# except the number at the index
+# touch every number in the list
+# return a list of products
+
+# when iterating over the input array, the expected value at the current
+# iteration index needs to be the product of every number except at the
+# current iteration index
+
+# while loop where the base case is when counter == len(arr)
+# while lop keeps iterating through for loop
+# for loop through the array multiplying all the numbers except for
+# the one at index counter. Append those to a list
+# everytime you loop through the numbers you incremement counter by 1
+# setting if x is == to the last element x[-1] in the array you incremeent counter
+# may need to append the product as a variable to a list
+# return the list of products
+#
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    counter = 0
+    products = []
+    result = 1
+    while counter != len(arr)-1:
+        for i, x in enumerate(arr):
+            if arr[i] == arr[counter]:
+                continue
+            if i == len(arr)-1:
+                result *= x
+                products.append(result)
+                result = 1
+                counter += 1
+            else:
+                result *= x
+    for i, x in enumerate(arr):
+        if arr[i] == arr[counter]:
+            products.append(result)
+            continue
+        if i == len(arr)-1:
+            result *= x
+            products.append(result)
+            result = 1
+            counter += 1
+        else:
+            result *= x
+    
+    return products
 
-    for x in arr:
-        for y + 1 in arr:
-            if arr[x] == arr[y]:
-                
-            x * y
+    # counter = 0
+    # products = []
+    # result = 1
+    # while counter <= len(arr):
+    #     for i, x in enumerate(arr):
+    #         if arr[i] == arr[counter]:
+    #             continue
+    #         if arr[i] == len(arr):
+    #             result *= x
+    #             products.append(result)
+    #             result = 1
+    #             counter += 1
+    #         else:
+    #             result *= x
 
-        if x == 0:
-            arr[x:] * arr[x:]
-        elif x == len(arr):
-            arr[:x]
-
-        return arr
 
 
 if __name__ == '__main__':
@@ -25,3 +76,15 @@ if __name__ == '__main__':
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    # for x in arr:
+    #     for y in arr:
+    #         if arr[x] == arr[y]:
+                
+    #         x * y
+
+    #     if x == 0:
+    #         arr[x:] * arr[x:]
+    #     elif x == len(arr):
+    #         arr[:x]
+
+    #     return arr
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,62 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
+#what if k is larger than the list of nums
+# does the returned array need to be sorted
+#window shouldn't exceed array length, so moving only so many
+# moves one index over each time with total window size not to exceed array length
+# finds the max of each window
+# append the max to a list
+# shift window
 
-    pass
+def sliding_window_max(nums, k, products=[], counter=0):
+    # Your code here
+    window = k
+    if counter < (len(nums) - k + 1):
+        list1 = nums[counter:window]
+        products.append(max(list1))
+        counter += 1
+        return sliding_window_max(nums, k, products, counter, window + 1)
+    return products
+
+
+    
+l = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+sliding_window_max(l, k)
+'''
+    Solution that utilizes a Queue
+    We'll maintain the invariant that the front element of the Queue will be the max value of the window at all times
+    O(n) time, O(k) space
+    '''
+    from collections import deque
+    maxs = []
+    q = deque()
+    for i, n in enumerate(nums):
+        # remove elements from the Queue so long as the current number is greater that the element
+        while len(q) > 0 and n > q[-1]:
+            q.pop()
+        # add the number once all smaller numbers have been evicted from the Queue
+        q.append(n)
+        # calculate the window range 
+        window = i - k + 1
+        # so long as the window's range == k, we'll add elements to the Queue
+        if window >= 0:
+            # add the max element, in this case the first element in the Queue, to the output List 
+            maxs.append(q[0])
+            # check if the number on the left side of the window is going to be evicted in the next iteration
+            # if it is, and if that value is at the front of the Queue, we need to evict it from the Queue
+            if nums[window] == q[0]:
+                q.popleft()
+    return maxs
+    # counter = 0
+    # n = k    
+    # if counter <= (len(nums) - n):
+    #     list1 = nums[counter:k]
+    #     max(list1)
+    #     counter += 1
+    #     sliding_window_max(nums, k+1)
+
 
 
 if __name__ == '__main__':
